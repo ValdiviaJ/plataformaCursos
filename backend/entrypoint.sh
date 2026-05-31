6484adbaf0702139552df6a890db354b7ec57413
Valdivia J <valdiviaangel731@gmail.com>
2026-05-31 18:17:50 -0500

    fix: run migrate:fresh in entrypoint to programmatically reset schema and apply correct tables

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -28,8 +28,8 @@ if (!\$dbReady) {
 }
 "
 
-echo "Ejecutando php artisan migrate --force..."
-php artisan migrate --force
+echo "Ejecutando php artisan migrate:fresh --force..."
+php artisan migrate:fresh --force
 
 echo "Ejecutando php artisan db:seed --force..."
 php artisan db:seed --force
